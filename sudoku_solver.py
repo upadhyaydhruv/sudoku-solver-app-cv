057e4b7ec2c3949dfcfb3838866328a7bab910bb
Dhruv Upadhyay <dmupadhyay12@gmail.com>
2020-07-22 10:35:15 -0400

    Add files via upload

---
+++ sudoku_solver.py
@@ -0,0 +1,77 @@
+import numpy as np
+import cv2 as cv
+import sys
+
+#The code below implements the sudoku solver, assuming it's a nine by nine grid (2 dimensional array - list of lists)
+
+#Sample sudoku grid
+grid = [
+    [5,3,0,0,7,0,0,0,0],
+    [6,0,0,1,9,5,0,0,0],
+    [0,9,8,0,0,0,0,6,0],
+    [8,0,0,0,6,0,0,0,3],
+    [4,0,0,8,0,3,0,0,1],
+    [7,0,0,0,2,0,0,0,6],
+    [0,6,0,0,0,0,2,8,0],
+    [0,0,0,4,1,9,0,0,5],
+    [0,0,0,0,8,0,0,7,9]
+    ]
+
+# this returns whether or not a number can be placed at a given position in the grid
+def possible_placed(y, x, n):
+    global grid
+
+    for i in range(0,9):
+        if grid[y][i] == n:
+            return False
+    
+    for i in range(0,9):
+        if grid[i][x] == n:
+            return False
+    
+    x0 = (x//3)*3
+    y0 = (y//3)*3
+
+    for i in range(3):
+        for j in range(3):
+            if grid[y0+i][x0+j]==n:
+                return False
+    
+    return True
+
+
+#This function solves the sudoku grid using a recursive bactracking algorithm
+
+def solve():
+    global grid
+
+    for y in range(9):
+        for x in range(9):
+            if grid[y][x] == 0:
+                for n in range(1,10):
+                    if possible_placed(y,x,n):
+                        grid[y][x] = n
+                        solve() 
+                        grid[y][x] = 0
+                return
+    print(np.matrix(grid))
+
+#solve()   
+
+
+file_path = "D:\Dhruv\sudoku-solver-test.jpg"
+img = cv.imread(file_path, 0) 
+if img is None:
+    sys.exit("Could not read the image.")
+
+gauss_blurred = cv.GaussianBlur(img, (5,5), cv.BORDER_DEFAULT) #Applying Gaussian blur on image to get rid of unnecessary noise 
+
+adaptive_threshold = cv.adaptiveThreshold(gauss_blurred, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 3) #
+
+adaptive_threshold = cv.bitwise_not(adaptive_threshold)
+
+kernel = np.ones((3,3),np.uint8)
+dilated = cv.dilate(adaptive_threshold, kernel, iterations = 1)
+
+cv.imshow("display", dilated)
+k=cv.waitKey(0)
